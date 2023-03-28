dea85f1d265c62ed4ef96ae50e68d5fe43b6765a
Wendy <50072566+vendaciki@users.noreply.github.com>
2023-03-28 15:55:07 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ Dejte pozor na správné formátování, kdy jednociferné hodnoty je zvykem zob
 
 const minutka = (e) => {
 	e.preventDefault()
-	let min = Number(document.querySelector('.time-input').value)
+	let min = Number(document.querySelector(".time-input").value)
 	let sec = 0
 
-	const minuty = document.querySelector('.alarm__minutes')
-	minuty.textContent = String(min) + ':'
+	const minuty = document.querySelector(".alarm__minutes")
+	minuty.textContent = String(min) + ":"
 
-	const sekundy = document.querySelector('.alarm__seconds')
-	sekundy.textContent = String(sec).padStart(2, '0')
+	const sekundy = document.querySelector(".alarm__seconds")
+	sekundy.textContent = String(sec).padStart(2, "0")
 
-	const alarm = document.querySelector('.alarm')
-	alarm.classList.remove('alarm--ring')
+	const alarm = document.querySelector(".alarm")
+	alarm.classList.remove("alarm--ring")
 
 	const odpocet = () => {
 		if (min >= 0) {
@@ -27,17 +27,17 @@ const minutka = (e) => {
 				min -= 1
 				sec = 59
 			}
-			minuty.textContent = String(min) + ':'
-			sekundy.textContent = String(sec).padStart(2, '0')
+			minuty.textContent = String(min) + ":"
+			sekundy.textContent = String(sec).padStart(2, "0")
 		}
 		if (min <= 0 && sec <= 0) {
 			clearInterval(casovac)
-			alarm.classList.add('alarm--ring')
-			document.querySelector('audio').play()
+			alarm.classList.add("alarm--ring")
+			document.querySelector("audio").play()
 		}
 	}
 	const casovac = setInterval(odpocet, 1000)
 }
 
-const formular = document.querySelector('.controls')
-formular.addEventListener('submit', minutka)
+const formular = document.querySelector(".controls")
+formular.addEventListener("submit", minutka)
